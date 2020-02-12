d0f689955659f31b540c417420cdd6644337cc3f
MatiasPineda <matias.pinedav@gmail.com>
2020-02-12 18:37:17 -0300

    validation + style

--- generateQR.js
+++ generateQR.js
@@ -4,10 +4,15 @@ var pass = document.getElementById('pass');
 var hidden = document.getElementById('hidden')
 var qrcode = new QRCode(document.getElementById('qrcode'));
 
+encryption.addEventListener('change',function() {nopassHide()}, false);
+document.getElementById('generate').addEventListener('click',function(){checkRequired()},false);
+
+//Generates QR Code whit the data pulled from the form
 function generateQR() {
   var qrssid = ssid.value;
   var qrencryption = encryption.value;
   var qrpass = pass.value;
+  if(qrpass === "nopass") {qrpass=""}
   var qrhidden = hidden.checked;
 
   var wifi = 'WIFI:S:' + qrssid +
@@ -16,4 +21,23 @@ function generateQR() {
               ';H:' + qrhidden;
 
   qrcode.makeCode(wifi);
+}
+
+//Checks if the ssid and pass inputs are valid, meaning they have something in them
+function checkRequired() {
+  if(ssid.validity.valid === true && pass.validity.valid === true){
+    generateQR()
+  }
+}
+
+function nopassHide() {
+  var showpass = document.getElementById('showpass');
+  if (encryption.value === "nopass") {
+    showpass.style.display = 'none';
+    pass.required = false
+  }
+  else{
+    showpass.style.display = 'initial';
+    pass.required = true;
+  }
 }
